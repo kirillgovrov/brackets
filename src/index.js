e829dac108b89649c30dc9e9dac002372149c07c
kirillgovrov <kirylgowr@gmail.com>
2024-10-22 21:34:21 +0200

    add: task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const objSearch = Object.fromEntries(bracketsConfig);
+  let stack = [];
+
+  for (const char of str){
+    if (objSearch[char]) {
+      if (objSearch[char] === char && stack[stack.length-1]===char){
+      stack.pop();
+      } else {
+      stack.push(char);
+      }
+
+    } else {
+      const closSym = stack.pop();
+      if (objSearch[closSym] !== char) {
+        return false;
+      }
+    }
+  } 
+  return stack.length===0;
 }
